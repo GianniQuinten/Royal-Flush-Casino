bd9db9fc77bb8b761ecb6e38cc409a170656f0cb
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-02-23 17:17:55 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -26,6 +26,11 @@ namespace Royal_Flush_Casino
 
 			// Call the PrintMessage method
 			sportBetting.PrintMessage();
+
+			// quinten even voor jou (wist dit ook niet precies
+			// als je een class aanroept die niet static is dan maak je eerst een instance zoals hier
+			// wat ik zei over gebruik gwn print message werkt alleen als je expliciet aangeeft dat de method (dit geval sportbetting) static is.
+			// als je die create instance dus weghaalt en je zet public static void neer bij sportbetting dan werkt dat
 		}
 	}
 }
