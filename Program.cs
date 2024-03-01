44982110657cbf3ab1e0877607a326b79f323fac
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2024-03-01 14:22:55 +0100

    git add . is not working

--- Program.cs
+++ Program.cs
@@ -20,17 +20,7 @@ namespace Royal_Flush_Casino
 			// Call the EnterCasino method
 			/*royalFlushCasino.enterCasino(player);*/
 
-
-			// Create an instance of SportBetting
-			SportBetting sportBetting = new SportBetting();
-
-			// Call the PrintMessage method
-			sportBetting.PrintMessage();
-
-			// quinten even voor jou (wist dit ook niet precies
-			// als je een class aanroept die niet static is dan maak je eerst een instance zoals hier
-			// wat ik zei over gebruik gwn print message werkt alleen als je expliciet aangeeft dat de method (dit geval sportbetting) static is.
-			// als je die create instance dus weghaalt en je zet public static void neer bij sportbetting dan werkt dat
-		}
-	}
+            SportBetting.PrintMessage();
+        }
+    }
 }
