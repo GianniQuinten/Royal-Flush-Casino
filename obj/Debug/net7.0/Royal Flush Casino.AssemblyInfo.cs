b4e97c3fd05b7b927af4f41eba6f5fb81b03e5c2
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2024-04-12 17:03:02 +0200

    Added documentation for the deliverable assigment

--- obj/Debug/net7.0/Royal Flush Casino.AssemblyInfo.cs
+++ obj/Debug/net7.0/Royal Flush Casino.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Royal Flush Casino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e7fa3dd50cf59bf042c6779f71688499f17864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256e7ec5f3f3bf8b9e175a23ee31ec9aeb7cfd70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Royal Flush Casino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Royal Flush Casino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
